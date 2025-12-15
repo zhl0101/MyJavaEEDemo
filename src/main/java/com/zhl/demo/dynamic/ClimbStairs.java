b483fbc17cbddb364d482fed0e60fc7d3b86fbbe
zhl0101 <1985656933@qq.com>
2025-12-15 22:15:25 +0800

    ###算法练习-爬楼梯

---
+++ src/main/java/com/zhl/demo/dynamic/ClimbStairs.java
@@ -0,0 +1,26 @@
+package com.zhl.demo.dynamic;
+
+public class ClimbStairs {
+    // 假设你正在爬楼梯。需要 n 阶你才能到达楼顶。
+    //
+    //每次你可以爬 1 或 2 个台阶。你有多少种不同的方法可以爬到楼顶呢？
+    public int climbStairs(int n) {
+        if (n <= 2) {
+            return n;
+        }
+        int[] dp = new int[n + 1];
+        dp[1] = 1;
+        dp[2] = 2;
+        for (int i = 3; i <= n; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2];
+        }
+        return dp[n];
+
+    }
+    public static void main(String[] args) {
+        ClimbStairs climbStairs = new ClimbStairs();
+        int n = 5;
+        int res = climbStairs.climbStairs(n);
+        System.out.println(res);
+    }
+}
