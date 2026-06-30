53efb4830bc412d9bc4ae1470af35c2eaab03442
zhl <zhl_9401@163.com>
2026-06-30 11:41:48 +0800

    - 完成 LeetCode 21 题目要求的算法实现

---
+++ src/main/java/com/zhl/demo/leetCode/Solution21.java
@@ -0,0 +1,55 @@
+package com.zhl.demo.leetCode;
+
+/**
+ * @author zhl
+ * @version 1.0.0
+ * @Description TODO 21. 合并两个有序链表
+ * @createTime 2026年06月30日 09:14
+ */
+public class Solution21 {
+//    将两个升序链表合并为一个新的 升序 链表并返回。新链表是通过拼接给定的两个链表的所有节点组成的。
+
+    static class ListNode {
+        int val;
+        ListNode next;
+        ListNode() {}
+        ListNode(int val) { this.val = val; }
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        ListNode dummy = new ListNode(-1);
+        ListNode cur = dummy;
+        while (list1 != null && list2 != null) {
+            if (list1.val < list2.val) {
+                cur.next = list1;
+                list1 = list1.next;
+            } else {
+                cur.next = list2;
+                list2 = list2.next;
+            }
+            cur = cur.next;
+        }
+        cur.next = list1 == null ? list2 : list1;
+        return dummy.next;
+    }
+
+    public static void main(String[] args) {
+        Solution21 solution21 = new Solution21();
+        ListNode list1 = new ListNode(1);
+        list1.next = new ListNode(2);
+        list1.next.next = new ListNode(4);
+        ListNode list2 = new ListNode(1);
+        list2.next = new ListNode(3);
+        list2.next.next = new ListNode(4);
+        ListNode listNode = solution21.mergeTwoLists(list1, list2);
+        while (listNode != null) {
+            System.out.println(listNode.val);
+            listNode = listNode.next;
+        }
+    }
+
+
+}
